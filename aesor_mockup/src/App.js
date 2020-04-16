ea139dcc3cf4f0c80029273add1dc7a8953e6f8f
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-04-15 22:07:14 -0400

    added header, footer, and slide components with styles

--- aesor_mockup/src/App.js
+++ aesor_mockup/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-     
+      <Header />
+      <Footer />
+
+      
     </div>
   );
 }
