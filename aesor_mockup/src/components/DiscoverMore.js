c8a7ae2d7c5a2e8c4a92cf9964212d9e90cabdff
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-04-15 23:01:03 -0500

    updated DiscoverMore component

--- aesor_mockup/src/components/DiscoverMore.js
+++ aesor_mockup/src/components/DiscoverMore.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 function ProductTile() {
   return (
-    <div className="discoverMoreWrap">
-     
+    <div className="outsideDiscoverWrap">
+        <img alt='travel gift kit' src='../assets/images/Aesop-Gift-Kits-2019-20-Hybris-Range-Page-Secondary-Mid-Desktop-1690x945px.png'/>
+        <div className="discoverMoreWrap">
+        <h1 id='discoverMoreTitle'>Discover more</h1>
+        
+        </div>        
     </div>
   );
 }
