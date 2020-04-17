9412975ecb6b0bc6d93c2517be4eeb32b2b96283
Olga_Sanni <dev.sannikov@gmail.com>
2020-04-16 23:04:35 -0400

    components.css

--- aesor_mockup/src/components/SpacesOfDistinction.js
+++ aesor_mockup/src/components/SpacesOfDistinction.js
@@ -5,23 +5,25 @@ class SpacesOfDistinction extends React.Component{
 
     render(){
         return(
-           <div>
-               <div>
-                <div>
-                    <p>Architecture and Design</p>
-                    <h4>Spaces of Distinction</h4>
+           <div id="spacesMain">
+               <div id="topspacesDiv">
+                <div id="topspacesLeft">
+                    <p className="topspacesP">Architecture and Design</p>
+                    <p id="spacesHeader">Spaces of Distinction</p>
                 </div>  
-                <div>
-                    <p>Aesop stores are informed by the history, fabric and context of their local environments, yet retain an aesthetic consistency.</p>
-                    <button type="button">Find a nearby store <img src={require("../assets/images/arrow.png")} alt="arrow"/></button>
+                <div id="topspacesRight">
+                    <p className="topspacesP">Aesop stores are informed by the history, fabric and context of their local environments, yet retain an aesthetic consistency.</p>
+                    <button type="button">Find a nearby store &rarr; </button>
                 </div>
                 </div>
                <div>
                    <img id="spaceMainImg" src={require("../assets/images/Aesop-Bleecker-Street-Carousel-1-Desktop-2560x1440px.jpg")} alt="picture"/>
+                   <div id="bottomSpacesDiv">
                    <p>1 of 5</p>
                    <div>
                        <p>Aesop Bleecker Street</p>
                        <p>341 Bleecker St, New York, NY 10014</p>
+                   </div>
                    </div>
                </div>
 
